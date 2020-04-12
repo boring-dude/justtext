893989cfa5e8f043539c5e3ee88591b497899598
Aravindhan <parvind97@gmail.com>
2020-04-12 19:24:03 +0530

    Add session and authenticity

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,30 @@
 # frozen_string_literal: true
 
+# sessions controller
 class SessionsController < ApplicationController
   def new; end
+
+  def create
+    user = User.find_by(name: params[:session][:name])
+    if user&.authenticate(params[:session][:password])
+      create_session(user)
+    else
+      flash.now[:error] = 'Invalid credentials'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'Logged out successfully'
+    redirect_to login_path
+  end
+
+  private
+
+  def create_session(user)
+    session[:user_id] = user.id
+    flash[:success] = 'Logged in successfully'
+    redirect_to root_path
+  end
 end
